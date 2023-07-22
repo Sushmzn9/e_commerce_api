7fbcbf0f472f568636a9bba9c2819bc970c3c770
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-07-22 15:40:22 +1000

    update

--- src/helper/nodemailer.js
+++ src/helper/nodemailer.js
@@ -1,12 +1,10 @@
-// 1. smtp config
-// email body
-// send method
-
 import nodemailer from "nodemailer";
+// 2. email body
+// 3. send method
 
 export const accountVerificationEmail = async (obj) => {
-  const { email } = obj;
-
+  const { email, fName, link } = obj;
+  // 1. smtp config
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
@@ -18,27 +16,33 @@ export const accountVerificationEmail = async (obj) => {
 
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: `"E Store 👻" ${process.env.SMTP_USER}`, // sender address
+    from: `"EST Store " <${process.env.SMTP_USER}>`, // sender address
     to: email, // list of receivers
-    subject: "Account activation required ✔", // Subject line
-    text: `Hello ${fName}, please follow to activate your link`,
-    html: `<p>
+    subject: "Account activation required", // Subject line
+    text: `hello ${fName}, please follow the link to activate your account. ${link}`, // plain text body
+    html: `
+    <p>
     Hello ${fName}
 </p>
-
 <p>
-    Please follow the link below to activate your account
+please follow the link below to activate your account.
 </p>
-<br/>
-<br/>
+<br />
+<br />
 <p>
-    <a href="">
-    ${link}
-    </a>
+   <a href=${link}>  ${link} </a>
+</p>
+<br />
+<br />
 
-</p>`, // html body
+<p>
+    Regareds, <br />
+    EST Store <br />
+    Customer Support Team
+</p>
+    
+    `, // html body
   });
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 };
